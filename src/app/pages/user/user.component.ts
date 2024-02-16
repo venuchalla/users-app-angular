a3fed938a06c16d669ecb17491461d883902d7bd
“Venu” <Venuchalla1993@gmail.com>
2024-02-16 02:57:19 -0600

    added iframe

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  showIframe: boolean = false;
+  trustedUrl : any;
+  private sanitizer = inject(DomSanitizer)
+  constructor(){
+    this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl("")
+  }
+  public openIframe() {
+    console.log("user clicked on button");
+    this.showIframe=true;
+  }
+  closeIframe(){
+    this.showIframe=false;
+  }
 }
