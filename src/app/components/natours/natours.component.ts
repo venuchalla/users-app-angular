c33ce744a671854cd2fa18f83823453d94266541
“Venu” <Venuchalla1993@gmail.com>
2024-02-20 10:32:37 -0600

    removed commented code

--- src/app/components/natours/natours.component.ts
+++ src/app/components/natours/natours.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { NatoursRoutingModule } from './natours-routing.module';
 
-//standalone: true,
-//imports: [],
+
 @Component({
   selector: 'app-natours',
   standalone: true,
