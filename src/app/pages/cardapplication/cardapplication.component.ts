b9bb1bfca474ce502e2cd4f64a4f16c012606175
“Venu” <Venuchalla1993@gmail.com>
2026-04-06 21:10:38 -0500

    updated

--- src/app/pages/cardapplication/cardapplication.component.ts
+++ src/app/pages/cardapplication/cardapplication.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import {  MatCardModule } from '@angular/material/card';
 import { MatToolbar } from "@angular/material/toolbar";
-import { HeaderComponent } from "@app/components/header/header.component";
-import { FooterComponent } from "@app/components/footer/footer.component";
+import { HeaderComponent } from "@components/header/header.component";
+import { FooterComponent } from "@components/footer/footer.component";
 
 @Component({
   selector: 'app-cardapplication',
